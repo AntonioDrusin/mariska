2553ab8425d0c2e07cffdf856f6ab2fd1554ce0d
Antonio Drusin <antonio.drusin@gmail.com>
2015-10-11 20:35:41 -0400

    Added some stuff to the sprite sheet. Re-did "Level1" (AKA test level)

---
+++ core/levels/Mariska Tileset.tsx
@@ -0,0 +1,339 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Basic" tilewidth="32" tileheight="32">
+ <image source="../tiles/Tileset.png" width="512" height="512"/>
+ <tile id="1">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="67">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="96">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="97">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="101">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="102">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="103">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="112">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="113">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="114">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="115">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="128">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="129">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="130">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="131">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="144">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="145">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="146">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="147">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="148">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="149">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="150">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="151">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="160">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="161">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="162">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="163">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="176">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="177">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="178">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+ <tile id="179">
+  <properties>
+   <property name="Block" value=""/>
+  </properties>
+ </tile>
+</tileset>
